0fda6fbfb616fc9d3be0953cf94a3894bf3e8161
Murillo Cecilio <murilloceciliomoreira@gmail.com>
2023-04-03 13:57:05 -0300

    att nomes das img

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ class Produto {
     salvar() {
         let produto = this.lerDados();
 
-        if(this.validaCampos(produto)) {
+        if (this.validaCampos(produto)) {
             this.adicionar(produto);
         }
 
@@ -17,10 +17,11 @@ class Produto {
 
     listaTabela() {
         let tbody = document.getElementById('tbody');
+        tbody.innerText = '';//comando para não refazer a lista a cada add item.
 
-        for(let i = 0;i < this.arrayProdutos.length; i++) {
+        for (let i = 0; i < this.arrayProdutos.length; i++) {
             let tr = tbody.insertRow();
-            
+
             let td_id = tr.insertCell();
             let td_produto = tr.insertCell();
             let td_valor = tr.insertCell();
@@ -29,12 +30,15 @@ class Produto {
             td_id.innerText = this.arrayProdutos[i].id;
             td_produto.innerText = this.arrayProdutos[i].nomeProduto;
             td_valor.innerText = this.arrayProdutos[i].preco;
-            
 
+            td_id.classList.add('center');
+
+            let imgEdit = document.createElement('img');
+            imgEdit.src = 'img/edit'
 
         }
     }
-    
+
     adicionar(produto) {
         this.arrayProdutos.push(produto);
         this.id++;
@@ -55,11 +59,11 @@ class Produto {
     validaCampos(produto) {
         let msg = '';
 
-        if(produto.nomeProduto == '') {
+        if (produto.nomeProduto == '') {
             msg += 'Informe o nome do Produto \n';
         }
 
-        if(msg != '') {
+        if (msg != '') {
             alert(msg);
             return false
         }
