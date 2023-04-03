052abaee44ac818db6d3339f2a8ea553caca7fe8
Murillo Cecilio <murilloceciliomoreira@gmail.com>
2023-04-03 09:29:05 -0300

    criando o esqueleto do index, e estilizando page

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 class Produto {
+    //Atributos que será definido dentro do bloco.
     constructor() {
-
+        this.id = 0;
+        this.nomeProduto = '';
+        this.valor = 0;
     }
-
+    //Funções, ações que o projeto vai realizar
     adicionar() {
         alert('Vamos adicionar um produto');
+    }
+
+    excluir() {
+        alert('Este item será deletado')
     }
 }
 
